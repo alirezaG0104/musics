917297de726f4b150d0b41ae12f5fa9a4769b8fb
CatInBasket <ferdinandus.renaldi@gmail.com>
2022-03-24 21:46:05 +0700

    add icon

--- ListLagu/index.js
+++ ListLagu/index.js
@@ -19,25 +19,35 @@ function generateSound(status){
     	var item = document.createElement('li');
         item.appendChild(document.createTextNode(track[i]["path"]));
         item.id= track[i]["nama"];
-        item.onclick=function(){favorite(this.id)};
-        console.log(item);
+        item.onclick=function(){switchLagu(track[i]["path"])};
+        console.log(item);        
         listSound.append(item);
-    }
+        var icon= document.createElement('img');
+        icon.src="media/pngwing.com.png";
+        icon.className="icon";
+        icon.onclick=function(){favorite(item.id)};
+        listSound.append(icon);
+    }   
     
     return track;
 }
 
 function generateFav(){
     var listSound=document.getElementById("fav");
-    listSound=document.getElementById("fav");
+    //listSound=document.getElementById("fav");
     listSound.innerHTML="";
     for(let i=0;i<fav.length;i++){
     	var item = document.createElement('li');
         item.appendChild(document.createTextNode(fav[i]["path"]));
         item.id= fav[i]["nama"];
-        item.onclick=function(){unfavorite(this.id)};
+        item.onclick=function(){switchLagu(fav[i]["path"])};
         console.log(item);
         listSound.append(item);
+        var icon= document.createElement('img');
+        icon.src="media/pngwing.com.png";
+        icon.className="icon";
+        icon.onclick=function(){unfavorite(item.id)};
+        listSound.append(icon);
     }    
     return fav;
 }
@@ -91,6 +101,10 @@ function unfavorite(title){
     return track;
 }
 
+function switchLagu(link){
+    let mp3player=document.getElementById("trackplayer2");
+    mp3player.src=link;
+}
 const lagu=[{"nama":"s1","path":"s1.mp3"},{"nama":"s2","path":"s2.mp3"},{"nama":"s3","path":"s3.mp3"}];
 const laguSub=[{"nama":"s1full","path":"s1full.mp3"},{"nama":"s2full","path":"s2full.mp3"},{"nama":"s3full","path":"s3full.mp3"}];
 
